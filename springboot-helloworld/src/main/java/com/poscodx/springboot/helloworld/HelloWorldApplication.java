5668d403d5633055405f02af34246e3d0aaca7c4
dudco0040 <dudco0040@gmail.com>
2024-06-25 09:13:52 +0900

    6/24 springboot helloworld #01

--- springboot-helloworld/src/main/java/com/poscodx/springboot/helloworld/HelloWorldApplication.java
+++ springboot-helloworld/src/main/java/com/poscodx/springboot/helloworld/HelloWorldApplication.java
@@ -16,7 +16,14 @@ public class HelloWorldApplication {
 	public static void main(String[] args) {
 		/**
 		 * SpringApplication.run(...) 안에서 일어나는 일
-		 * 
+		 * 1. 어플리케이션 컨텍스(Application Context, Spring Container) 생성
+		 * 2. 만약, 웹 어플리케이션이면 웹 어플리케이션 타입 결정(Spring MVC, Reactive)
+		 * 3. Annotation scanning + Configuration Class를 통해서 빈 생성/등록/와이어링 작업
+		 * 4. 만약, 웹 어플리케이션이고 타입이 Spring MVC이면 
+		 * 	 - 내장(embeded) 서버(TomcatEmbededServiceContainer) 인스턴스 생성
+		 * 	 - 서버 인스턴스에 웹 어플리케이션을 배포
+		 * 	 - 서버 인스턴스를 실행 -> tomcat 서버가 뜸
+		 * 5. ApplicationRunner 인스페이스를 구현한 빈을 APpplication Context에서 찾아서  
 		 */
 		try(ConfigurableApplicationContext ac = SpringApplication.run(HelloWorldApplication.class, args)){
 				
